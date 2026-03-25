59f77876dc02278f9fe896f24ab6c29132e35ddd
adi-pat-node <patnaikaditya@gmail.com>
2026-03-25 20:29:55 +0530

    Uncomment OfficeMap component

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
       {/* <MissionStatement /> */}
       <ProgramOverview />
       <DeckSection />
-      {/* <OfficeMap /> */}
+      <OfficeMap />
       <FellowshipBenefits />
       <KeyStatistics />
       <Changemakers />
