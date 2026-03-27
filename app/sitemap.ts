d4c8230bc1198575eeb33a1a5c9fcfb24e1623bf
adi-pat-node <patnaikaditya@gmail.com>
2026-03-27 13:01:00 +0530

    Remove 'lander' URL entry from sitemap
    Removed the entry for the 'lander' URL from the sitemap.

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -47,11 +47,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "monthly",
       priority: 0.8,
     },
-    {
-      url: `${baseUrl}/lander`,
-      lastModified: currentDate,
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
   ];
 }
