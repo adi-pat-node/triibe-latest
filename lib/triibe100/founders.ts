7a9b7f83f776adbf370f24c3d32534cd2b63ee46
Pranay Mishra <btech11024.24@bitmesra.ac.in>
2026-06-18 12:51:07 +0530

    added 15 triibe 100 members

--- lib/triibe100/founders.ts
+++ lib/triibe100/founders.ts
@@ -35,19 +35,87 @@ const firstTen: Founder[] = [
     bio: "",
     image: "/images/triibe100/Shrusti Amula.png",
   },
-  { id: 5, name: "Name", org: "Non Profit", bio: "Coming soon." },
-  { id: 6, name: "Name", org: "Non Profit", bio: "Coming soon." },
-  { id: 7, name: "Name", org: "Non Profit", bio: "Coming soon." },
-  { id: 8, name: "Name", org: "Non Profit", bio: "Coming soon." },
-  { id: 9, name: "Name", org: "Non Profit", bio: "Coming soon." },
-  { id: 10, name: "Name", org: "Non Profit", bio: "Coming soon." },
+  {
+    id: 5,
+    name: "Sharona Shnayder",
+    org: "Tuesdays for Trash",
+    bio: "",
+    image: "/images/triibe100/Sharona Shnayder.png",
+  },
+  {
+    id: 6,
+    name: "Sayed Zubaer Hasan",
+    org: "Krishi Shwapno",
+    bio: "",
+    image: "/images/triibe100/Sayed-Zubaer-Hasan 2.png",
+  },
+  {
+    id: 7,
+    name: "Samin Bhan",
+    org: "Lookup",
+    bio: "",
+    image: "/images/triibe100/Samin Bhan.png",
+  },
+  {
+    id: 8,
+    name: "Rimza Razaque",
+    org: "Al Arzen",
+    bio: "",
+    image: "/images/triibe100/Rimza Razaque.png",
+  },
+  {
+    id: 9,
+    name: "Rachel Parent",
+    org: "Kids Right to Know",
+    bio: "",
+    image: "/images/triibe100/Rachel Parent.png",
+  },
+  {
+    id: 10,
+    name: "Olivia Zhang",
+    org: "Cancer Kids First",
+    bio: "",
+    image: "/images/triibe100/Olivia Zhang.png",
+  },
 ];
 
-const rest: Founder[] = Array.from({ length: 90 }, (_, i) => ({
-  id: i + 11,
-  name: "Name",
-  org: "Non Profit",
-  bio: "Coming soon.",
-}));
+const rest: Founder[] = [
+  {
+    id: 11,
+    name: "Nils Rauscher",
+    org: "Liberating Education Organization",
+    bio: "",
+    image: "/images/triibe100/Nils Rauscher.png",
+  },
+  {
+    id: 12,
+    name: "Nayla Jimenez",
+    org: "Philippine Health Initiative",
+    bio: "",
+    image: "/images/triibe100/Nayla Jimenez.png",
+  },
+  {
+    id: 13,
+    name: "Mirus Ponon",
+    org: "ASEAN Youth Advocates Network (AYAN)",
+    bio: "",
+    image: "/images/triibe100/Mirus Ponon.png",
+  },
+  {
+    id: 14,
+    name: "Mathias Charles Yabe",
+    org: "AkoFresh",
+    bio: "",
+    image: "/images/triibe100/Mathias Charles Yabe.png",
+  },
+  {
+    id: 15,
+    name: "Maria Keller",
+    org: "Read Indeed",
+    bio: "",
+    image: "/images/triibe100/Maria Keller.png",
+  },
+  
+];
 
 export const founders: Founder[] = [...firstTen, ...rest];
