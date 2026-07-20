d9f434d5ad0d9b04a1bebdfdbd7181d78f17b780
adi-pat-node <patnaikaditya@gmail.com>
2026-07-20 20:32:24 +0530

    Update description in About page metadata

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,7 @@ import AboutClient from "./AboutClient";
 export const metadata = {
   title: "About TRIIBE | Empowering Next-Gen Nonprofit Innovators",
   description:
-    "TRIIBE's global board, advisors & team providing institutional credibility, mentorship & networks to nonprofit innovators.",
+    "TRIIBE's global board, advisors & team providing institutional credibility, mentorship & networks to nonprofit founders.",
   alternates: {
     canonical: "https://www.triibe.org/about",
   },
