80bafbf4fbfcbc28b2f4e136ae914d621e040bd8
Pranay Mishra <btech11024.24@bitmesra.ac.in>
2026-09-03 10:03:51 +0530

    chore: added 360 one firm as our partner

--- components/keyStatistics.tsx
+++ components/keyStatistics.tsx
@@ -204,6 +204,12 @@ const partneredWithLogos = [
     url: "https://www.axiompathways.org/",
     className: "w-80 h-[100px] object-contain",
   },
+  {
+    src: "/images/home/360onefirm.png",
+    alt: "Partnered with organization 3",
+    url: "https://361firm.com/",
+    className: "w-80 h-[100px] object-contain",
+  },
   /* {
     src: "/images/home/unitedyouthcouncil.png",
     alt: "Partnered with organization 3",
